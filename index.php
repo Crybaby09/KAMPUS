f8a6728dfea8d44a72061b5ae0c6d1e5948e0aa4
Crybaby <115837653+Crybaby09@users.noreply.github.com>
2023-01-09 15:20:20 +0700

    edited ke 2
    ganti table dengan include php

--- index.php
+++ index.php
@@ -23,39 +23,8 @@
 
 <body>
     <header class="sticky-top">
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">Home</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Mata Kuliah
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="page1.php">Pengantar Manajemen</a></li>
-                                <li><a class="dropdown-item" href="page2.php">Algoritma</a></li>
-                                <li><a class="dropdown-item" href="page3.php">Operating System</a></li>
-                                <li><a class="dropdown-item" href="page4.php">Bahasa Web</a></li>
-                                <li><a class="dropdown-item" href="page5.php">Pengantar TI</a></li>
-                                <li><a class="dropdown-item" href="page6.php">Basis Data</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <!-- include navbar -->
+        <?php include "./assets/template/navbar.php"; ?>
     </header>
     <main>
         <div class="container">
